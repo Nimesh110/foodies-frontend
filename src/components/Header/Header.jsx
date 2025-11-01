d6d547d6c900e83fcb8cbb3e07e1518bc3f70699
Vidura Nimesh <emoneyvn.info@gmail.com>
2025-11-01 21:29:36 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+     <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum reiciendis dolores reprehenderit ea iste quis facilis vitae ducimus delectus quasi fugiat illo, sunt veniam magni a. Illo laudantium autem dicta.</p>
+        <button>View Menu</button>
+        </div> 
+    </div>
+  )
+}
+
+export default Header
